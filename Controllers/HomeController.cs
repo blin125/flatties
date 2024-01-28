bdeeb4bfe9c868fcc82a8ff4b23d287faa633f13
Bruno William <blin125@aucklanduni.ac.nz>
2024-01-28 15:59:49 +1300

    replaced Azure AD to Azure AD B2C, also not deployed anymore, Authentication works but bug - Cookies remain in header causing error after signin signout 4 ish times

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,35 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Flatties.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Diagnostics;
 
-namespace Flatties.Controllers;
-
-[Authorize]
-public class HomeController : Controller
+namespace Flatties.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    [Authorize]
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        if (!User.Identity.IsAuthenticated){
-            _logger.LogInformation("User is not authenticated."); // Testing purposes
-            return RedirectToAction("SignIn", "Account");
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
-        return View();
-    }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+
+            return View(errorViewModel);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,31 +1,30 @@
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureADB2C"));
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to 
+    // the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+builder.Services.AddRazorPages(options => {
+    // options.Conventions.AllowAnonymousToPage("/Index");
+})
+.AddMvcOptions(options => { })
+.AddMicrosoftIdentityUI();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Add authentication services
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
-    .EnableTokenAcquisitionToCallDownstreamApi()
-    .AddInMemoryTokenCaches();
-
-builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
-{
-    options.Events.OnRedirectToIdentityProvider = context =>
-    {
-        context.ProtocolMessage.RedirectUri = context.Request.Scheme + "://" + context.Request.Host + "/";
-        return Task.CompletedTask;
-    };
-
-    options.Events.OnRedirectToIdentityProviderForSignOut = context =>
-    {
-        context.ProtocolMessage.PostLogoutRedirectUri = context.Request.Scheme + "://" + context.Request.Host + "/Account/SignOut";
-        return Task.CompletedTask;
-    };
-});
+builder.Logging.AddConsole(); // Add logging to the console
 
 var app = builder.Build();
 
@@ -33,21 +32,21 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 app.UseRouting();
 
-// Add authentication middleware
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Restrict the entire app to authenticated users
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .RequireAuthorization();
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    
+    endpoints.MapRazorPages();
+});
 app.Run();
