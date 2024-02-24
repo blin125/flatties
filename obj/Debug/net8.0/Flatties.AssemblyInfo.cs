877d6fd180e3ac1c452e24cc82836611f47376f1
Bruno William <blin125@aucklanduni.ac.nz>
2024-02-24 16:29:09 +1300

    login and register pages backend and front end work - need adjustments on the css part, specially in the register, as the width does not math login's

--- obj/Debug/net8.0/Flatties.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flatties.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flatties")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30399e66ee0b05a8ffc119405e369c039745135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435a68aab9d50b3af618ae2c86ac011d6725eb0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flatties")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flatties")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
